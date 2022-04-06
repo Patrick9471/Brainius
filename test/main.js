dc3109d4c1c79bad0e13a42442ecbd1b8f7bd5fe
patrick9471 <azezbek@mail.ru>
2022-04-06 09:55:08 +0300

    first commit

---
+++ test/main.js
@@ -0,0 +1,24 @@
+$(function(){
+	$('.rev_slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+	});
+	$('.form_slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+	});
+	$('.vid_slider').slick({
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 1500,
+	})
+
+})
